c592a23fb740f30fb53061fbbdc3c1d4a13a64b5
Cristina Chaparro <cricbau@gmail.com>
2023-04-27 18:00:39 +0200

    footer done

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,20 @@
+import Image from "next/image";
+
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="border-t-2 w-full flex justify-between items-center p-3">
+      <Image src="/homeally.png" alt="home icon" width={40} height={40} />
+      <Image
+        src="/notificationsally.png"
+        alt="notifications icon"
+        width={40}
+        height={40}
+      />
+      <Image src="/addally.png" alt="add icon" width={40} height={40} />
+      <Image src="/saveally.png" alt="save icon" width={40} height={40} />
+      <Image src="/profileally.png" alt="profile icon" width={40} height={40} />
+    </div>
+  );
 }
 
 export default Footer;
