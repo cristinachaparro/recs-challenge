3bc3ead3150ab6a76a42d0305c611bf6b5b1495f
Cristina Chaparro <cricbau@gmail.com>
2023-04-27 18:35:34 +0200

    improved footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,34 @@
 import Image from "next/image";
+import Link from "next/link";
 
 function Footer() {
   return (
     <div className="border-t-2 w-full flex justify-between items-center p-3">
-      <Image src="/homeally.png" alt="home icon" width={40} height={40} />
-      <Image
-        src="/notificationsally.png"
-        alt="notifications icon"
-        width={40}
-        height={40}
-      />
-      <Image src="/addally.png" alt="add icon" width={40} height={40} />
-      <Image src="/saveally.png" alt="save icon" width={40} height={40} />
-      <Image src="/profileally.png" alt="profile icon" width={40} height={40} />
+      <Link href="/">
+        <Image src="/homeally.png" alt="home icon" width={40} height={40} />
+      </Link>
+      <Link href="/notifications">
+        <Image
+          src="/notificationsally.png"
+          alt="notifications icon"
+          width={40}
+          height={40}
+        />
+      </Link>
+      <Link href="/add-post">
+        <Image src="/addally.png" alt="add icon" width={40} height={40} />
+      </Link>
+      <Link href="/saved-posts">
+        <Image src="/saveally.png" alt="save icon" width={40} height={40} />
+      </Link>
+      <Link href="/user-profile">
+        <Image
+          src="/profileally.png"
+          alt="profile icon"
+          width={40}
+          height={40}
+        />
+      </Link>
     </div>
   );
 }
